c10a8c4479ca41cd1b4d4c3bf1f208a5c5d8327e
Myles <myo3@pitt.edu>
2019-10-26 02:14:54 -0400

    First Draft

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+
+function toggleSidebar(){
+    document.getElementById("sidebar").classList.toggle('active');
+}
+
+function openAbout(){
+    document.getElementById("view").src= "about.html";
+}
+
+function openSocial(){
+    document.getElementById("view").src = "socialdeterminants.html";
+}
+
+function openMore(){
+    document.getElementById("view").src = "more.html";
+}
